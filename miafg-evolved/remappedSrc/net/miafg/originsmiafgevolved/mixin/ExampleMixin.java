f7dc7a2b8d09f4eb6e37dcfb68d5086b92b96878
LarryPlayz <romanblundell@gmail.com>
2023-03-28 22:11:59 +0100

    Updated

---
+++ miafg-evolved/remappedSrc/net/miafg/originsmiafgevolved/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.miafg.originsmiafgevolved.mixin;
+
+import net.miafg.originsmiafgevolved.EvolvedMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EvolvedMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
